93057d3fedb240fa9e1900163b81a4ef017405ab
rebelwarrior <rabbit.editor@gmail.com>
2019-06-19 16:25:31 -0400

    adds begining of a constant

---
+++ elm/ConstantBuilder.elm
@@ -0,0 +1,36 @@
+module ConstantBuilder exposing (..)
+
+{-
+Uses title 
+Uses new data 
+Uses new currency (integer for placing decimal place when converting to string.)
+-}
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+buildConstant question =
+    let 
+        num = 
+            String.fromInt question.data 
+            |> formatDecimal question.decimalPlaces
+        
+    in 
+    div [] 
+    [ div [] [text question.title] 
+    , div [] [text num ]
+    ]
+
+formatDecimal : Int -> String -> String 
+formatDecimal decimalPlaces stringNum  =
+    let 
+        decimals = 
+            String.right decimalPlaces stringNum 
+        
+        num = 
+            String.dropRight decimalPlaces stringNum 
+
+    in 
+    case decimals of 
+        "" -> num 
+        _  -> num ++ "." ++ decimals 
